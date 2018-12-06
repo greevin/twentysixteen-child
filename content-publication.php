a9c2c7cd7a5195fc478c89d5e446dd54bed98d02
Cícero Neto <cicero.alex@gmail.com>
2018-12-06 09:24:20 -0200

    Removido link que direcionava para as pessoas

--- content-publication.php
+++ content-publication.php
@@ -25,7 +25,7 @@
                 if( $posts ): ?>
                     <?php foreach( $posts as $post ): // variable must be called $post (IMPORTANT) ?>
                         <?php setup_postdata($post); ?>
-                        <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a><br>
+                        <?php the_title(); ?><br>
                     <?php endforeach; ?>
                     <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
                 <?php endif; ?>
