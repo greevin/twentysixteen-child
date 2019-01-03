6d2ce6b2fc28ffc0e64503f9fd718340aed68f25
Cícero Neto <cicero.alex@gmail.com>
2019-01-03 14:00:23 -0200

    Adicionado jumbotron para as páginas internas

--- assets/includes/jumbotron.php
+++ assets/includes/jumbotron.php
@@ -2,10 +2,9 @@
 // Jumbotron para as páginas internas
 ?>
 <section class="pagina-interna">
-  <div class="jumbotron jumbotron-fluid text-center">
-    <div class="container">
-      <h2 class="display-4 jumbotron-title"><?php the_title(); ?></h2>
-      <h5><?php the_field('autor') ?></h5>
+  <div class="jumbotron jumbotron-fluid">
+    <div class="">
+      <h2 class="display-4 jumbotron-title" style="padding-left: 3.5%;"><?php the_title(); ?></h2>
     </div>
   </div>
 </section>
--- page-equipe.php
+++ page-equipe.php
@@ -5,12 +5,12 @@ get_header();
 
 <!-- equipes -->
 <div class="equipe pagina-inicial" style="background: white;">
-  <div class="container">
+  <div class="">
     <div class="equipe-atual section-title">
       <header class="entry-header">
-		      <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <?php include(STYLESHEETPATH . '/assets/includes/jumbotron.php'); ?>
 	    </header><!-- .entry-header -->
-      <div class="row">
+      <div class="row page-content">
         <?php
            $args = array('post_type' => 'equipe', 'post_status' => 'publish', 'posts_per_page' => -1, 'orderby'=> 'title', 'order' => 'ASC');
            $posts = get_posts($args);
