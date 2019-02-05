236755f7ff17a1482e15a2cffbeac9a4a47e01da
Cícero Neto <cicero.alex@gmail.com>
2019-02-05 10:52:24 -0200

    Alterado tamanho do título das páginas

--- assets/includes/jumbotron.php
+++ assets/includes/jumbotron.php
@@ -4,7 +4,7 @@
 <section class="pagina-interna">
   <div class="jumbotron jumbotron-fluid">
     <div class="">
-      <h2 class="display-4 jumbotron-title" style="padding-left: 3.5%;"><?php the_title(); ?></h2>
+      <h2 class="display-4 jumbotron-title" style="padding-left: 3.5%;font-size: 2.5rem;"><?php the_title(); ?></h2>
     </div>
   </div>
 </section>
